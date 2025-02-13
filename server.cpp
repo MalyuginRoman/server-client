e65d4b963a1c5bf401820f544ec3f916679ffb6f
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-13 17:10:02 +0500

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -29,112 +29,97 @@ int main(void)
     inet_pton(AF_INET, SERVER_IP, &ip_to_num);
 
 
-    // WinSock initialization
-    WSADATA wsData;
-    erStat = WSAStartup(MAKEWORD(2,2), &wsData);
-
-    if (erStat != 0) {
-        cout << "Error WinSock version initializaion #";
-        cout << WSAGetLastError();
-        return 1;
-    }
-    else
-        cout << "WinSock initialization is OK" << endl;
-
+//    // WinSock initialization
+//    WSADATA wsData;
+//    erStat = WSAStartup(MAKEWORD(2,2), &wsData);
+//    if (erStat != 0) {
+//        cout << "Error WinSock version initializaion #";
+//        cout << WSAGetLastError();
+//        return 1;
+//    }
+//    else
+//        cout << "WinSock initialization is OK" << endl;
     while (true)
     {
-
         // Socket initialization
         SOCKET ClientSock = socket(AF_INET, SOCK_STREAM, 0);
-
         if (ClientSock == INVALID_SOCKET) {
             cout << "Error initialization socket # " << WSAGetLastError() << endl;
             closesocket(ClientSock);
             WSACleanup();
         }
-
         // Establishing a connection to Server
         sockaddr_in servInfo;
-
         ZeroMemory(&servInfo, sizeof(servInfo));
-
         servInfo.sin_family = AF_INET;
         servInfo.sin_addr = ip_to_num;
         servInfo.sin_port = htons(SERVER_PORT_NUM);
 
-        erStat = connect(ClientSock, (sockaddr*)&servInfo, sizeof(servInfo));
-
-        if (erStat != 0) {
-            cout << "Connection to Server is FAILED. Error # " << WSAGetLastError() << endl;
-            closesocket(ClientSock);
-            WSACleanup();
-            return 1;
-        }
-        //else
-        //    cout << "Connection established SUCCESSFULLY. Ready to send a message to Server" << endl;
-
+//        erStat = connect(ClientSock, (sockaddr*)&servInfo, sizeof(servInfo));
+//        if (erStat != 0) {
+//            cout << "Connection to Server is FAILED. Error # " << WSAGetLastError() << endl;
+//            closesocket(ClientSock);
+//            WSACleanup();
+//            return 1;
+//        }
+//        //else
+//        //    cout << "Connection established SUCCESSFULLY. Ready to send a message to Server" << endl;
 
         //Exchange text data between Server and Client. Disconnection if a Client send "xxx"
-
         vector <char> servBuff(BUFF_SIZE), clientBuff(BUFF_SIZE);							// Buffers for sending and receiving data
-        short packet_size = 0;												// The size of sending / receiving packet in bytes
+        short packet_size = 0;												                // The size of sending / receiving packet in bytes
 
         packet_size = recv(ClientSock, servBuff.data(), servBuff.size(), 0);
 
-        if (packet_size == SOCKET_ERROR) {
-            cout << "Can't receive message from Server. Error # " << WSAGetLastError() << endl;
-            closesocket(ClientSock);
-            WSACleanup();
-            return 1;
-        }
-        else
-            cout << servBuff.data();
-
+//        if (packet_size == SOCKET_ERROR) {
+//            cout << "Can't receive message from Server. Error # " << WSAGetLastError() << endl;
+//            closesocket(ClientSock);
+//            WSACleanup();
+//            return 1;
+//        }
+//        else
+//            cout << servBuff.data();
         fgets(clientBuff.data(), clientBuff.size(), stdin);
         packet_size = send(ClientSock, clientBuff.data(), clientBuff.size(), 0);
 
-        if (packet_size == SOCKET_ERROR) {
-            cout << "Can't send message to Server. Error # " << WSAGetLastError() << endl;
-            closesocket(ClientSock);
-            WSACleanup();
-            return 1;
-        }
-
+//        if (packet_size == SOCKET_ERROR) {
+//            cout << "Can't send message to Server. Error # " << WSAGetLastError() << endl;
+//            closesocket(ClientSock);
+//            WSACleanup();
+//            return 1;
+//        }
         packet_size = recv(ClientSock, servBuff.data(), servBuff.size(), 0);
 
-        if (packet_size == SOCKET_ERROR) {
-            cout << "Can't receive message from Server. Error # " << WSAGetLastError() << endl;
-            closesocket(ClientSock);
-            WSACleanup();
-            return 1;
-        }
-        else
-            cout << servBuff.data();
-
+//        if (packet_size == SOCKET_ERROR) {
+//            cout << "Can't receive message from Server. Error # " << WSAGetLastError() << endl;
+//            closesocket(ClientSock);
+//            WSACleanup();
+//            return 1;
+//        }
+//        else
+//            cout << servBuff.data();
         fgets(clientBuff.data(), clientBuff.size(), stdin);
         packet_size = send(ClientSock, clientBuff.data(), clientBuff.size(), 0);
 
-        if (packet_size == SOCKET_ERROR) {
-            cout << "Can't send message to Server. Error # " << WSAGetLastError() << endl;
-            closesocket(ClientSock);
-            WSACleanup();
-            return 1;
-        }
-
+//        if (packet_size == SOCKET_ERROR) {
+//            cout << "Can't send message to Server. Error # " << WSAGetLastError() << endl;
+//            closesocket(ClientSock);
+//            WSACleanup();
+//            return 1;
+//        }
         packet_size = recv(ClientSock, servBuff.data(), servBuff.size(), 0);
 
-        if (packet_size == SOCKET_ERROR) {
-            cout << "Can't receive message from Server. Error # " << WSAGetLastError() << endl;
-            closesocket(ClientSock);
-            WSACleanup();
-            return 1;
-        }
-        else
-            cout << servBuff.data() << endl;;
+//        if (packet_size == SOCKET_ERROR) {
+//            cout << "Can't receive message from Server. Error # " << WSAGetLastError() << endl;
+//            closesocket(ClientSock);
+//            WSACleanup();
+//            return 1;
+//        }
+//        else
+//            cout << servBuff.data() << endl;;
 
         closesocket(ClientSock);
     }
-    WSACleanup();
-
+//    WSACleanup();
     return 0;
 }
