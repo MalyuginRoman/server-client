81d06fd5139c2504fe8e17d11ca594594cabeddf
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-03 11:30:29 +0500

    Update server_b.cpp

--- server_b.cpp
+++ server_b.cpp
@@ -9,6 +9,14 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <list>
+
+//using namespace std;
+namespace fs = std::experimental::filesystem;
+
 std::vector <char> convert_string_to_char(std::vector <char> result, std::string s)
 {
     size_t l = s.length();
@@ -40,6 +48,96 @@ std::vector <char> clearBuf(std::vector <char> result)
     for(int i = BUFF_SIZE - 1; i > -1; i--)
         result.emplace(result.begin(), '\0');
     return result;
+}
+
+std::string split(std::string str, char del)
+{
+    std::string result = "";
+    std::string temp = "";
+    for(int i=0; i<(int)str.size(); i++)
+    {
+        if(str[i] != del)
+            temp += str[i];
+        else
+        {
+            result += temp + "/";
+            temp = "";
+        }
+    }
+    std::cout << std::endl;
+    return result;
+}
+
+std::list<std::list<std::string>> readGameConfig()
+{
+    fs::path current_path = fs::current_path();
+    fs::path file_path(current_path);
+    std::list<std::list<std::string>> list1;
+    // Рекурсивный обход директории
+    std::cout << file_path.parent_path() << std::endl;
+
+    char del = '/';
+    std::string directory = split(file_path.parent_path().generic_string(), del);
+    std::cout << directory << std::endl;
+
+    for (auto &p : fs::recursive_directory_iterator(directory))
+    {
+        std::string readFileName = directory + p.path().filename().generic_string();
+        std::fstream readFile(readFileName);
+        if (readFile.is_open())
+        {
+            std::cout << "File " << readFileName << " be opened" << std::endl;
+            std::list<std::string> list2;
+            list2.push_back(readFileName);
+            std::string str;
+            while(readFile >> str)
+            {
+                if(str == "Game" /*status = "*/)
+                {
+                    readFile >> str >> str >> str;
+                    list2.push_back(str);
+                }
+                else if(str == "Create" /*game with ID : "*/)
+                {
+                    readFile >> str >> str >> str >> str >> str;
+                    list2.push_back(str);
+                }
+                else if(str == "Number" /*players: "*/)
+                {
+                    readFile >> str >> str;
+                    list2.push_back(str);
+                }
+                else if(str == "first" /*: "*/)
+                {
+                    readFile >> str >> str;
+                    list2.push_back(str);
+                }
+                else if(str == "second" /*: "*/)
+                {
+                    readFile >> str >> str;
+                    list2.push_back(str);
+                }
+                else if(str == "third" /*: "*/)
+                {
+                    readFile >> str >> str;
+                    list2.push_back(str);
+                }
+                else if(str == "fourth" /*: "*/)
+                {
+                    readFile >> str >> str;
+                    list2.push_back(str);
+                }
+                else if(str == "fifth" /*: "*/)
+                {
+                    readFile >> str >> str;
+                    list2.push_back(str);
+                }
+            }
+            list1.push_back(list2);
+        }
+        readFile.close();
+    }
+    return list1;
 }
 
 
@@ -110,16 +208,29 @@ std::cout << "_________________________________________________" << std::endl;
     std::string answerNo  = "No" ;
     //--------------------------------------------------------------------------// Write in file
     QString fileName;                                                           //
-    int CurrentGameID = 0;                                                      // ID текущей игры (для записи в файл и конектов к ней)
-    std::string ToWriteInFile = std::to_string(CurrentGameID);                  //
-    fileName = "D:/Models/OTUS/server-client/game_status." +                    //
-                        QString::fromStdString(ToWriteInFile) + ".txt";         //
-    QFile file(fileName);                                                       //
-    if (!file.open( QIODevice::WriteOnly | QIODevice::Text )) {                 //
-        qDebug() << "Не удалось открыть файл»";                                 //
-        return 1;                                                               //
+    int CurrentGameID;                                                          // ID текущей игры (для записи в файл и конектов к ней)
+    //--------------------------------------------------------------------------//
+    std::string readFileName = "D:/Models/OTUS/server-client/game_count.txt";   // чтение количества созданных игр
+    std::fstream readFile(readFileName);                                        //
+    std::string str;                                                            //
+    if (readFile.is_open())                                                     //
+    {                                                                           //
+        readFile >> str;                                                        //
+        CurrentGameID = std::stoi(str);                                         //
     }                                                                           //
-    QTextStream stream(&file);                                                  //
+    else                                                                        //
+    {                                                                           //
+        CurrentGameID = 0;                                                      // если файла нет, то создаем
+        fileName = "D:/Models/OTUS/server-client/game_count.txt";               //
+        QFile file0(fileName);                                                  //
+        if (!file0.open( QIODevice::WriteOnly | QIODevice::Text )) {            //
+            qDebug() << "Не удалось открыть файл»";                             //
+            return 1;                                                           //
+        }                                                                       //
+        QTextStream stream0(&file0);                                            //
+        stream0 << CurrentGameID;                                               //
+        file0.close();                                                          //
+    }                                                                           //
     //--------------------------------------------------------------------------//
     bool isWok = true;
     while (isWok)                                                            // Цикл № 1 - для создания игра / определения наличия и формата доступа клиентов к игре
@@ -186,6 +297,17 @@ std::cout << "_________________________________________________" << std::endl;
 std::cout << "_________________________________________________" << std::endl;
 std::cout << "        Start creating GAME                      " << std::endl;
 std::cout << "_________________________________________________" << std::endl;
+            //--------------------------------------------------------------------------//
+            std::string ToWriteInFile = std::to_string(CurrentGameID);                  //
+            fileName = "D:/Models/OTUS/server-client/game_status." +                    //
+                                QString::fromStdString(ToWriteInFile) + ".txt";         //
+            QFile file(fileName);                                                       //
+            if (!file.open( QIODevice::WriteOnly | QIODevice::Text )) {                 //
+                qDebug() << "Не удалось открыть файл»";                                 //
+                return 1;                                                               //
+            }                                                                           //
+            QTextStream stream(&file);                                                  //
+            //--------------------------------------------------------------------------//
             clientBuff = clearBuf(clientBuff);
             answer = "Number players: ";
             clientBuff = convert_string_to_char(clientBuff, answer);
@@ -263,6 +385,7 @@ std::cout << "_________________________________________________" << std::endl;
             clientBuff = clearBuf(clientBuff);
             clientBuff = convert_string_to_char(clientBuff, answer);
             packet_size = send(ClientConn, clientBuff.data(), clientBuff.size(), 0);
+            stream << "Game status = 0" << endl;
             stream << "Create game with ID : " << QString::fromStdString(ToWriteInFile) << endl;
             QString strX = QString::fromStdString(numberPlayers);
             stream << "Number players: " << strX << endl;
@@ -284,12 +407,24 @@ std::cout << "_________________________________________________" << std::endl;
                 return 1;
             }
             CurrentGameID ++;
+        //--------------------------------------------------------------------------//
+            fileName = "D:/Models/OTUS/server-client/game_count.txt";               // обновляем файл с количеством игр
+            QFile file0(fileName);                                                  //
+            if (!file0.open( QIODevice::WriteOnly | QIODevice::Text )) {            //
+                qDebug() << "Не удалось открыть файл»";                             //
+                return 1;                                                           //
+            }                                                                       //
+            QTextStream stream0(&file0);                                            //
+            stream0 << CurrentGameID;                                               //
+            file0.close();                                                          //
+        //--------------------------------------------------------------------------//
+            file.close();
         }
         else if(isCreating == answerNo)
         {
             //...               // connecting to game
+            answer = "Connection to Game? ";
             clientBuff = clearBuf(clientBuff);
-            answer = "Connection to Game?";
             clientBuff = convert_string_to_char(clientBuff, answer);
             std::cout << clientBuff.data();
             packet_size = send(ClientConn, clientBuff.data(), clientBuff.size(), 0);
@@ -302,25 +437,110 @@ std::cout << "_________________________________________________" << std::endl;
                 return 1;
             }
             packet_size = recv(ClientConn, servBuff.data(), servBuff.size(), 0);
+            if (packet_size == SOCKET_ERROR) {
+                std::cout << "Can't receive message from Server. Error # " << WSAGetLastError() << std::endl;
+                closesocket(ClientConn);
+                WSACleanup();
+                return 1;
+            }
+            else
+                std::cout << servBuff.data();
             std::string isConection;
             isConection = convert_char_to_string(servBuff, isConection);
             if(isConection == answerYes)  // при подключении к игре пользователь прошел верификацию
             {
                 // ....
+                answer = "Write game ID: ";
+                clientBuff = clearBuf(clientBuff);
+                clientBuff = convert_string_to_char(clientBuff, answer);
+                std::cout << clientBuff.data();
+                packet_size = send(ClientConn, clientBuff.data(), clientBuff.size(), 0);
+                if (packet_size == SOCKET_ERROR)
+                {
+                    std::cout << "Can't send message to Client. Error # " << WSAGetLastError() << std::endl;
+                    closesocket(ServSock);
+                    closesocket(ClientConn);
+                    WSACleanup();
+                    return 1;
+                }
+                packet_size = recv(ClientConn, servBuff.data(), servBuff.size(), 0);
+                if (packet_size == SOCKET_ERROR) {
+                    std::cout << "Can't receive message from Server. Error # " << WSAGetLastError() << std::endl;
+                    closesocket(ClientConn);
+                    WSACleanup();
+                    return 1;
+                }
+                else
+                    std::cout << servBuff.data();
+                std::string isGameID;
+                isGameID = convert_char_to_string(servBuff, isGameID);
+                int gameID = stoi(isGameID);
+
                 answer = "Write your name: ";
                 clientBuff = clearBuf(clientBuff);
                 clientBuff = convert_string_to_char(clientBuff, answer);
+                std::cout << clientBuff.data();
                 packet_size = send(ClientConn, clientBuff.data(), clientBuff.size(), 0);
+                if (packet_size == SOCKET_ERROR)
+                {
+                    std::cout << "Can't send message to Client. Error # " << WSAGetLastError() << std::endl;
+                    closesocket(ServSock);
+                    closesocket(ClientConn);
+                    WSACleanup();
+                    return 1;
+                }
+                packet_size = recv(ClientConn, servBuff.data(), servBuff.size(), 0);
+                if (packet_size == SOCKET_ERROR) {
+                    std::cout << "Can't receive message from Server. Error # " << WSAGetLastError() << std::endl;
+                    closesocket(ClientConn);
+                    WSACleanup();
+                    return 1;
+                }
+                else
+                    std::cout << servBuff.data();
                 std::string UserName;
                 UserName = convert_char_to_string(servBuff, UserName);
-                bool isPlayerConection = false;
-                for(int i = 0; i < numPlayers; i++)
-                {
-                    if(UserName == NamePlayers.at(i))
-                        isPlayerConection = true;
-                }
-                if(isPlayerConection == false)
-                    break;
+                //--------------------------------------------------------------------------//
+                std::list<std::list<std::string>> list_game_config = readGameConfig();      // чтение файлов с конфигарацией игр
+                std::cout << list_game_config.size() << std::endl;                          //
+                while(!list_game_config.empty())                                            //
+                {                                                                           //
+                    int read_count = 0;                                                     //
+                    while(!list_game_config.front().empty())                                //
+                    {                                                                       //
+                        if(read_count == 0)
+                            std::cout << "File name: ";
+                        else if(read_count == 1)
+                            std::cout << "Game status = ";
+                        else if(read_count == 2)
+                            std::cout << "Create game with ID: ";
+                        else if(read_count == 3)
+                            std::cout << "Number players: ";
+                        else if(read_count == 4)
+                            std::cout << "first: ";
+                        else if(read_count == 5)
+                            std::cout << "second: ";
+                        else if(read_count == 6)
+                            std::cout << "third: ";
+                        else if(read_count == 7)
+                            std::cout << "fourth: ";
+                        else if(read_count == 8)
+                            std::cout << "fifth: ";
+                        std::cout << list_game_config.front().front() << std::endl;         //
+                        list_game_config.front().pop_front();                               //
+                        read_count++;                                                       //
+                    }                                                                       //
+                    list_game_config.pop_front();                                           //
+                }                                                                           //
+                //--------------------------------------------------------------------------//
+                //bool isPlayerConection = false;
+                //for(int i = 0; i < numPlayers; i++)
+                //{
+                //    if(UserName == NamePlayers.at(i))
+                //        isPlayerConection = true;
+                //}
+                //if(isPlayerConection == false)
+                //    break;
             }
             else if(isConection == answerNo)  // при подключении к игре пользователь не прошел верификацию
             {
@@ -337,7 +557,6 @@ std::cout << "_________________________________________________" << std::endl;
             WSACleanup();
             return 1;
         }
-        file.close();
         closesocket(ClientConn);
     }
     closesocket(ServSock);
