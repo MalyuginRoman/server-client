0b3cb08a2a7badaf8939cc8d4890e8a8b04f7603
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-07 17:22:33 +0500

    Create order.h

---
+++ order.h
@@ -0,0 +1,41 @@
+#ifndef ORDER_H
+#define ORDER_H
+#include <string>
+#include <vector>
+
+class order
+{
+public:
+    order(int playerID, int objectID, std::string actionName, std::string specParam);
+    ~order();
+
+    int playerID() const;
+    int objectID() const;
+    std::string actionName() const;
+    std::string specParam() const;
+
+    void setObjectID(int value);
+    void setActionName(std::string value);
+    void setSpecParam(std::string value);
+
+private:
+    class orderP* imp;
+};
+
+class orderVector
+{
+public:
+    orderVector();
+    ~orderVector();
+    void reset();
+
+    order* add(int playerID, int objectID, std::string actionName, std::string specParam);
+    bool isEmpty() const;
+    size_t count() const;
+    const std::vector<order *>& vector() const;
+    order* at(int i);
+private:
+    class orderVectorP* imp;
+};
+
+#endif // ORDER_H
