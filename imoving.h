8cba9cd672e7060e6aceb8a88f482ee803cb62e6
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-07 17:21:06 +0500

    Create imoving.h

---
+++ imoving.h
@@ -0,0 +1,14 @@
+#ifndef IMOVING_H
+#define IMOVING_H
+class object;
+
+class IMoving
+{
+public:
+    virtual bool getPosition(object *obj, int dt) = 0;
+    virtual bool setPosition(object *obj) = 0;
+    virtual bool getVelocity(object *obj, double du) = 0;
+    virtual bool setVelocity(object *obj) = 0;
+};
+
+#endif // IMOVING_H
