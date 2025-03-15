4135b9ee1a098c287010aaa68ecdcf3b3db98c08
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-15 18:54:18 +0500

    Create game_creator.h

---
+++ game_creator.h
@@ -0,0 +1,23 @@
+#ifndef GAME_CREATOR_H
+#define GAME_CREATOR_H
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <QString>
+#include <QDir>
+#include <QDebug>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#include "dop_function.h"
+#include "minor.h"
+#include <vector>
+
+class game_creator
+{
+public:
+    void default_games(std::vector<game> *games);
+    bool game_create(std::vector<game> *games);
+};
+
+#endif // GAME_CREATOR_H
