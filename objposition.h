bfdf0d4a255ccf78fc491d0280217af8b05175c3
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-15 18:56:25 +0500

    Update objposition.h

--- objposition.h
+++ objposition.h
@@ -8,9 +8,11 @@ class object_position
 {
 public:
     double local_ext(double f1, double f2, int num, int count);
-    bool isXY(std::map<int, system_okr> p_map, objectVector *vector, int i, int j);
+    bool isXY(std::map<int, system_okr> *p_map, objectVector *vector, int i, int j);
+    bool isXY_r(std::map<int, system_okr> *p_map, object *obj, int j);
     std::map<int, system_okr> func_name(int number);
-    std::map<int, system_okr> func_obj(std::map<int, system_okr> p_map, objectVector *vector);
+    std::map<int, system_okr> *func_obj(std::map<int, system_okr> *p_map, objectVector *vector);
+    std::map<int, system_okr> *func_obj_r(std::map<int, system_okr> *p_map, object *obj);
 };
 
 #endif // OBJPOSITION_H
