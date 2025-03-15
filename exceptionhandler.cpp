c35a8c416df39815dfb276118947d88781a675d3
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-15 18:58:14 +0500

    Create exceptionhandler.cpp

---
+++ exceptionhandler.cpp
@@ -0,0 +1,42 @@
+#include "ExceptionHandler.h"
+#include "icommand.h"
+
+class ExceptionHandlerP
+{
+public:
+    ICommand* cmd;
+    std::exception ex;
+    ExceptionHandlerP(ICommand* cmd, std::exception ex) :
+        cmd(nullptr),
+        ex(ex)
+    {
+    }
+};
+
+ExceptionHandler::ExceptionHandler(ICommand* cmd, std::exception ex) :
+    imp(new ExceptionHandlerP(cmd, ex))
+{
+}
+
+ExceptionHandler::~ExceptionHandler() { delete imp; }
+
+void ExceptionHandler::executeRepeat(ExceptionHandler* handler, SafeQueue<ICommand *> *cmd, std::exception ex)
+{
+    std::cout << "Repeat execute of ExceptionHandler" << ex.what() << std::endl;
+    cmd->push(cmd->front());
+    cmd->pop();
+    try {
+        cmd->front()->execute();
+        cmd->pop();
+    } catch( std::exception ex1) {
+        handler->executeWrite(cmd, ex1);
+    }
+}
+
+void ExceptionHandler::executeWrite(SafeQueue<ICommand *> *cmd, std::exception ex)
+{
+    std::cout << "Write execute of ExceptionHandler" << ex.what() << std::endl;
+    LogerCommand *cmd_loger = new LogerCommand;
+    cmd_loger->execute();
+    cmd->pop();
+}
