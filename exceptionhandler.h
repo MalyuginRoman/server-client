b14a98a80ea9446954e2632915488ff043db29bb
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-15 18:53:58 +0500

    Create exceptionhandler.h

---
+++ exceptionhandler.h
@@ -0,0 +1,22 @@
+#ifndef EXCEPTIONHANDLER_H
+#define EXCEPTIONHANDLER_H
+#include <iostream>
+#include "icommand.h"
+#include "safequeue.h"
+
+class ExceptionHandler
+{
+public:
+    ExceptionHandler(ICommand* cmd, std::exception ex);
+    ~ExceptionHandler();
+
+    ICommand* cmd() const;
+    std::exception ex() const;
+
+    void executeWrite(SafeQueue<ICommand *> *cmd, std::exception ex);
+    void executeRepeat(ExceptionHandler* handler, SafeQueue<ICommand *> *cmd, std::exception ex);
+private:
+    class ExceptionHandlerP* imp;
+};
+
+#endif // EXCEPTIONHANDLER_H
