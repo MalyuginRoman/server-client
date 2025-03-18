ed5fb6ee1633fa9547f719dec3af855ec849f02f
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-18 16:22:23 +0500

    Create minor.cpp

---
+++ minor.cpp
@@ -0,0 +1,25 @@
+#include "minor.h"
+
+int readFuelConfig()
+{
+    int dF = 0;
+    fs::path current_path = fs::current_path();
+    fs::path file_path(current_path);
+    for (auto &p : fs::recursive_directory_iterator(file_path.parent_path()))
+    {
+        std::string current_file = p.path().filename().generic_string();
+        std::string readFileName = p.path().parent_path().generic_string() + "/" +
+                                   p.path().filename().generic_string();
+        if(current_file == "FuelConfige.txt")
+        {
+            std::fstream readFile(readFileName);
+            if (readFile.is_open())
+            {
+                std::string str;
+                readFile >> str;
+                dF = stoi(str);
+            }
+        }
+    }
+    return dF;
+}
