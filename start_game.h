90fc4feecb771aa5c0a0120a09d39bc25c823cb0
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-18 16:34:11 +0500

    Update start_game.h

--- start_game.h
+++ start_game.h
@@ -1,12 +1,11 @@
-#ifndef START_GAME_H
-#define START_GAME_H
+#pragma once
 #include <fstream>
 #include <filesystem>
 #include <string>
 #include <list>
-//#include "icommand.h"
 #include "objposition.h"
 #include "scope.h"
+#include "ioc.h"
 
 struct player
 {
@@ -18,8 +17,8 @@ struct player
 class start_game
 {
 public:
-    start_game(std::map<std::string, std::function<ICommand*()>> *m_map,
-               std::map<std::string, std::string> *m_scope,
+    start_game(std::map<std::string, std::function<ICommand*()>> m_map,
+               std::map<std::string, std::string> m_scope,
                std::vector<player> *player_list,
                objectVector *obj_vector);
     ~start_game();
@@ -28,7 +27,7 @@ public:
     std::vector<player> player_list;
     std::map<int, system_okr> p_map_c_a;
     std::map<int, system_okr> p_map_c_b;
-    objectVector obj_vector;
+    objectVector *obj_vector;
     std::map<int, system_okr> create_pole(int type);
     //scope my_scope;
     void create_player(std::string playerName,
@@ -36,11 +35,15 @@ public:
                        objectVector *obj_vector,
                        std::map<int, system_okr> *p_map_c_a,
                        std::map<int, system_okr> *p_map_c_b);
+    game_scope filling_scope(std::map<std::string, std::function<ICommand*()>> m_map,
+                             std::map<std::string, std::string> m_scope);
     void play_round(orderVector *player_messages,
                     std::vector<player> *player_list,
                     objectVector *obj_vector,
                     std::map<int, system_okr> *p_map_c_a,
-                    std::map<int, system_okr> *p_map_c_b);
+                    std::map<int, system_okr> *p_map_c_b,
+                    std::map<std::string, std::function<ICommand*()>> m_map,
+                    std::map<std::string, std::string> m_scope);
     collisionObjects check_collision(objectVector *obj_vector,
                         std::map<int, system_okr> *p_map_c_a,
                         std::map<int, system_okr> *p_map_c_b);
@@ -49,5 +52,3 @@ public:
 private:
     class gameP* imp;
 };
-
-#endif // START_GAME_H
