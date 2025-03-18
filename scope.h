ff8753a5348b45aa958aff11166cd288eaf52fa3
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-18 16:33:21 +0500

    Update scope.h

--- scope.h
+++ scope.h
@@ -1,5 +1,4 @@
-#ifndef SCOPE_H
-#define SCOPE_H
+#pragma once
 #include <iostream>
 #include <list>
 #include <map>
@@ -15,10 +14,6 @@ public:
     std::map<std::string, std::function<ICommand*()>>* m_map;
     std::map<std::string, std::string>* m_scope;
     void resolve(std::string key_s, std::string key_f, std::function<ICommand*()> func);
-    scope* fillin(scope *scope_old, std::map<int, system_okr> p_map_a,
-                 std::map<int, system_okr> p_map_b, objectVector *vector);
 private:
     class scopeP* imp;
 };
-
-#endif // SCOPE_H
