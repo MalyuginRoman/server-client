23d33e2c20c801ef354b73bf34510cb9dd783aa7
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-18 16:24:30 +0500

    Update scope.cpp

--- scope.cpp
+++ scope.cpp
@@ -29,36 +29,3 @@ void scope::resolve(std::string key_s, std::string key_f, std::function<ICommand
     imp->m_scope->emplace(key_s, key_f);
     imp->m_map->emplace(key_f, func);
 }
-
-scope *scope::fillin(scope *scope_old, std::map<int, system_okr> p_map_a, std::map<int, system_okr> p_map_b, objectVector *vector)
-{
-    scope *scope_new = scope_old;
-
-    scope_new->resolve("Scope1",
-                    "MoveCommand",
-                    [&p_map_a, &p_map_b, &vector]() {
-                    return new MoveCommand(&p_map_a, &p_map_b, vector->at(0)); });
-    scope_new->resolve("Scope1",
-                    "RotateCommand",
-                    [&vector]() { return new RotateCommand(vector->at(0)); });
-    scope_new->resolve("Scope1",
-                    "LogerCommand",
-                    []() { return new LogerCommand(); });
-    scope_new->resolve("Scope1",
-                    "EmptyCommand",
-                    []() { return new EmptyCommand(); });
-    scope_new->resolve("Scope1",
-                    "HardStopCommand",
-                    []() { return new HardStopCommand(); });
-    scope_new->resolve("Scope1",
-                    "SoftStopCommand",
-                    []() { return new SoftStopCommand(); });
-    scope_new->resolve("Scope1",
-                    "StartMotion",
-                    [&vector]() { return new StartMotion(vector->at(0), vector->at(0)->state().velocity); });
-    scope_new->resolve("Scope1",
-                    "StopMotion",
-                    [&vector]() { return new StopMotion(vector->at(0)); });
-
-    return scope_new;
-}
