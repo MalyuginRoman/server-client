c61ea1a428288889cbbb418f7ba8706429e333dd
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-18 16:28:04 +0500

    Update game_creator.h

--- game_creator.h
+++ game_creator.h
@@ -1,14 +1,4 @@
-#ifndef GAME_CREATOR_H
-#define GAME_CREATOR_H
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <stdio.h>
-#include <QString>
-#include <QDir>
-#include <QDebug>
-
-#pragma comment(lib, "Ws2_32.lib")
-
+#pragma once
 #include "dop_function.h"
 #include "minor.h"
 #include <vector>
@@ -19,5 +9,3 @@ public:
     void default_games(std::vector<game> *games);
     int game_create(std::vector<game> *games);
 };
-
-#endif // GAME_CREATOR_H
