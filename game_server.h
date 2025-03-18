fa00606bf7890fa480b6f8dc07229a4730d01552
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-18 16:28:27 +0500

    Update game_server.h

--- game_server.h
+++ game_server.h
@@ -1,16 +1,4 @@
-#ifndef GAME_SERVER_H
-#define GAME_SERVER_H
-//#include <iostream>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <stdio.h>
-#include <vector>
-#include <QString>
-#include <QDir>
-#include <QDebug>
-
-#pragma comment(lib, "Ws2_32.lib")
-
+#pragma once
 #include "dop_function.h"
 #include "start_game.h"
 #include "order.h"
@@ -23,5 +11,3 @@ public:
     bool game_server_main(std::vector<game> *games, int gameID, SOCKET ServSock);
     SOCKET bind_server();
 };
-
-#endif // GAME_SERVER_H
