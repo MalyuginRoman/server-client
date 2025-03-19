b8a00fc96cca7e745326322c67b3f513c2da03cd
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-03-19 09:46:12 +0500

    Update game_creator.cpp

--- game_creator.cpp
+++ game_creator.cpp
@@ -1,13 +1,13 @@
-#include <stdio.h> // standard input and output library
-#include <stdlib.h> // this includes functions regarding memory allocation
-#include <string.h> // contains string functions
-#include <errno.h> //It defines macros for reporting and retrieving error conditions through error codes
-#include <time.h> //contains various functions for manipulating date and time
-#include <unistd.h> //contains various constants
-#include <sys/types.h> //contains a number of basic derived types that should be used whenever appropriate
-#include <arpa/inet.h> // defines in_addr structure
-#include <sys/socket.h> // for socket creation
-#include <netinet/in.h> //contains constants and structures needed for internet domain addresses
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h> 
+#include <sys/types.h>
+#include <cstring>
+#include <vector>
 #include "game_creator.h"
 
 int game_creator::game_create(std::vector<game> *games)
